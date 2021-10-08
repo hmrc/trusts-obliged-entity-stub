8f44c552bba09b0ff70ef8510686598a2a29816e
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2021-10-08 13:25:43 +0100

    play-upgrade: Upgrade play to 28

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"    %% "bootstrap-backend-play-27" % "5.8.0",
+    "uk.gov.hmrc"    %% "bootstrap-backend-play-28" % "5.9.0",
     "com.github.fge"  % "json-schema-validator"     % "2.2.6"
   )
 
@@ -12,8 +12,8 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"          % "3.2.8" % "test",
     "com.typesafe.play"       %% "play-test"          % current % "test",
     "org.pegdown"             %  "pegdown"            % "1.6.0" % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play" % "4.0.3" % "test, it",
-    "com.vladsch.flexmark" %  "flexmark-all"          % "0.35.10" % "test"
+    "org.scalatestplus.play"  %% "scalatestplus-play" % "5.0.0" % "test, it",
+    "com.vladsch.flexmark" %  "flexmark-all"          % "0.35.10" % "test, it"
   )
 
   val akkaVersion = "2.6.14"
