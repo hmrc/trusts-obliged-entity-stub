23ad084aaad1fe546e3f486af32d454f2cf83303
Victor Osipov <osipovvictor1994@gmail.com>
2023-08-24 15:53:53 +0100

    DDCE-4110 - Upgrade deps, added scalastyle (#18)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,19 +1,21 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.19.0"
+  val bootstrapVersion = "7.21.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"    %% "bootstrap-backend-play-28" % bootstrapVersion,
     "com.github.fge"  % "json-schema-validator"     % "2.2.14"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.mockito"             % "mockito-core"              % "5.4.0",
+    "org.mockito"             % "mockito-core"              % "5.5.0",
     "org.scalatest"           %% "scalatest"                % "3.2.16",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
+
+  def apply(): Seq[ModuleID] = compile ++ test
 
 }
